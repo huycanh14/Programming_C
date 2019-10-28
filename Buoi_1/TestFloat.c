17c3db3d880ea2689b2d4763df426dc984ef0f96
Huy Canh <huycanh14@gmail.com>
2019-10-28 15:04:39 +0700

    test float

---
+++ Buoi_1/TestFloat.c
@@ -0,0 +1,11 @@
+#include <conio.h>
+#include <stdio.h>
+void main(){
+	float a = 987.56894695;
+//	printf("%f", a);
+//	printf("%10.f", a);
+//	printf("%0.f", a);
+//	printf("%.f", a);
+//	printf("%.5f", a);
+	printf("%.15f", a);
+}
